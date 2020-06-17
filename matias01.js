7e7c8a085c138ec4c801972fc90881a541ba7f08
MatuteDV <matiasvieira0@gmail.com>
2020-06-16 21:35:33 -0300

    Mensaje para Bruno en archivo matias01.js

--- matias01.js
+++ matias01.js
@@ -2,3 +2,4 @@ console.log('El texto agregado.');
 console.log("probando modificar el mismo archivo");
 let textos = {};
 textos.otroTexto = 'Este es otro texto';
+textos.charla = 'Grande Bruno, hace un pull para traer esto, luego modifica y hace otro push y quiero ver de resolver yo el error';

3b41be946b25ff9f4cbaac87ef4fe1e8d2352204
MatuteDV <matiasvieira0@gmail.com>
2020-06-16 21:50:52 -0300

    Probando agregar algo para que dé error

--- matias01.js
+++ matias01.js
@@ -3,3 +3,4 @@ console.log("probando modificar el mismo archivo");
 let textos = {};
 textos.otroTexto = 'Este es otro texto';
 textos.charla = 'Grande Bruno, hace un pull para traer esto, luego modifica y hace otro push y quiero ver de resolver yo el error';
+textos.mirespuesta = 'Esto va a quedar junto con lo que traigo de Bruno';
